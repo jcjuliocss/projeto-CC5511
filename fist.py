c1a1c7553851b0160f845010aada99a8e9463ecd
Julio Cesar <39426877+jcjuliocss@users.noreply.github.com>
2019-09-23 14:12:33 -0400

    Update fist.py

--- fist.py
+++ fist.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template
-from flask.views import Teste
+from flask import Flask
 
 t = Teste()
 
@@ -8,15 +7,3 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return "<b>Voce e um FANFARRAO</b>"
-
-@app.route('/teste')
-def teste():
-    return 'Teste'
-
-@app.route('/temp')
-def temp():
-    return render_template('hello.html')
-
-@app.route('/testar')
-def testar():
-    return t.testar()
