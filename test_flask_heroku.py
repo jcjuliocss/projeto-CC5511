cc63e51b2ffd80e842a3d3e3e78e844043e25bbe
Julio Cesar <39426877+jcjuliocss@users.noreply.github.com>
2019-09-23 14:19:05 -0400

    Update test_flask_heroku.py

--- test_flask_heroku.py
+++ test_flask_heroku.py
@@ -5,4 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<b>teste</b>"
-app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
