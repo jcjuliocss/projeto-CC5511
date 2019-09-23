701e49af6661dd6f24d151bdd1f347e1abebb61e
Julio Cesar <39426877+jcjuliocss@users.noreply.github.com>
2019-09-23 19:25:36 -0300

    Update test_flask_heroku.py

--- test_flask_heroku.py
+++ test_flask_heroku.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<b>JOÃO TRABALHADOR</b>"
+    return "<b>Kkkkkkkkkkkkkkkkkk</b>"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
