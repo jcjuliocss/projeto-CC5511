315621cce6e735f373e97f1fe6650250217e17cd
Julio Cesar <39426877+jcjuliocss@users.noreply.github.com>
2019-09-23 14:21:03 -0400

    Update test_flask_heroku.py

--- test_flask_heroku.py
+++ test_flask_heroku.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,4 +8,5 @@ def hello_world():
     return "<b>teste</b>"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
