ba0f048bb92a5d4c808b9c72e5707c1e2faefd34
Julio Cesar <julio.silva@softrh.com.br>
2019-09-27 14:12:58 -0400

    ajustando arquivo correto

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
+"""."""
 import os
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__, static_url_path='/static')
 
+usuarios_emails = []
+usuarios_senhas = []
+
+
 @app.route("/")
 def login():
+    """Pagina de login."""
     return render_template('login.html')
+
 
 @app.route("/index")
 def index():
+    """Pagina inicial."""
     return render_template('index_html.html')
+
 
 @app.route("/cadastro")
 def cadastro():
-	return render_template("signup.html")
+    """Pagina de cadastro."""
+    return render_template("signup.html")
+
 
 @app.route("/processar_cadastro", methods=['GET', 'POST'])
 def processar_cadastro():
-	return request.form.get("nome")
+    """Adiciona usuario globalmente."""
+    dados = request.form
+
+    usuarios_emails.append(dados.get("email"))
+    usuarios_senhas.append(dados.get("senha"))
+
+    return login()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+app.run(host='0.0.0.0', port=port)
