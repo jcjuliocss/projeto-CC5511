01622da48d1fef508018b454a14fa6a9a3fc0902
Julio Cesar <39426877+jcjuliocss@users.noreply.github.com>
2019-09-27 00:32:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def cadastro():
 
 @app.route("/processar_cadastro")
 def processar_cadastro():
-	return request.form
+	return request.form.get("nome")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
